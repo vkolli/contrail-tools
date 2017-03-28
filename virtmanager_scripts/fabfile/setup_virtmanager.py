258f9b9e59480aef4c2c9132f2a19b0f9c9ab2c5
Jeba Paulaiyan <jebap@juniper.net>
2017-03-28 15:21:39 -0700

    Point sources.list to point to server-manager repo

--- virtmanager_scripts/fabfile/setup_virtmanager.py
+++ virtmanager_scripts/fabfile/setup_virtmanager.py
@@ -183,10 +183,12 @@ def point_sources_list_smrepo(hostname,smip,reimage_param):
     with open('/etc/apt/sources.list') as infile, open('/etc/apt/sources.list.t', 'w') as outfile:
         for line in infile:
             line = re.sub('deb http[:/a-zA-Z0-9\.]+','deb http://'+smip+'/contrail/images/'+reimage_param,line)
-            line = re.sub('deb-src http[:/a-zA-Z0-9\.]+','deb-src http://'+smip+'/contrail/images/ubuntu-14.04.5',line)
+            line = re.sub('deb-src http[:/a-zA-Z0-9\.]+','deb-src http://'+smip+'/contrail/images/'+reimage_param,line)
             outfile.write(line)
         outfile.write('deb http://'+smip+'/thirdparty_packages/ ./\n')
-    run("mv /etc/apt/sources.list.t /etc/apt/sources.list")
+        run("cat /etc/apt/sources.list")
+	run("cat /etc/apt/sources.list.t")
+    run("cp /etc/apt/sources.list.t /etc/apt/sources.list")
 
 def change_host_name_of_vm(hostname,smip,reimage_param):
     host = hostname
