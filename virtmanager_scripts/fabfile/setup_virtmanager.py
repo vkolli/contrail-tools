31eb30c9f0f29fb908385aae68e8df08d62fa716
Jeba Paulaiyan <jebap@juniper.net>
2017-03-31 17:03:25 -0700

    point sources.list to smrepo

--- virtmanager_scripts/fabfile/setup_virtmanager.py
+++ virtmanager_scripts/fabfile/setup_virtmanager.py
@@ -117,7 +117,9 @@ def create_vms_from_testbed(contrail_fab_path='/opt/contrail/utils'):
         for i in range(0,3):
             try:
                 with settings(host_string=key):
-                    change_host_name_of_vm(vm_detail['name'],smip,reimage_param)
+                    change_host_name_of_vm(vm_detail['name'])
+                    point_sources_list_smrepo(host,smip,reimage_param)
+                    setup_ntp(host,smip)
                 break
             except :
                 time.sleep(30)
@@ -184,11 +186,12 @@ def setup_ntp(hostname,smip):
     run("service ntp restart")
 
 def point_sources_list_smrepo(hostname,smip,reimage_param):
+    f1='/tmp/'+hostname+'.sources_in.list'
+    f2='/tmp/'+hostname+'.sources.list'
     run("cp /etc/apt/sources.list /etc/apt/sources.list.image")
-    infile=open('/etc/apt/sources.list')
-    outfile=open('/etc/apt/sources.list.t', 'w')
-    print "infile "+infile
-    print "outfile "+outfile
+    get('/etc/apt/sources.list', '/tmp/'+hostname+'.sources.list')
+    infile=open(f1)
+    outfile=open(f2, 'w')
     for line in infile:
         print line
         line = re.sub('deb http[:/a-zA-Z0-9\.]+','deb http://'+smip+'/contrail/images/'+reimage_param,line)
@@ -197,17 +200,13 @@ def point_sources_list_smrepo(hostname,smip,reimage_param):
     outfile.write('deb http://'+smip+'/thirdparty_packages/ ./\n')
     infile.close()
     outfile.close()
-    run("cat /etc/apt/sources.list.t")
-    run("cat /etc/apt/sources.list")
-    run("cp /etc/apt/sources.list.t /etc/apt/sources.list")
+    put(f2,"/etc/apt/sources.list")
     run("apt-get update")
 
-def change_host_name_of_vm(hostname,smip,reimage_param):
+def change_host_name_of_vm(hostname):
     host = hostname
     generate_etc_hostname(host)
     generate_etc_hosts(host)
-    point_sources_list_smrepo(host,smip,reimage_param)
-    setup_ntp(host,smip)
     #run("cp /etc/hostname /etc/hostname.old")
     run("cp /etc/hosts /etc/hosts.old")
     put("hostname", "/etc/")
