23a3468f718cf89f70389294e04038426ad07050
SOUMIL KULKARNI <soumilk91@gmail.com>
2017-04-25 17:23:09 -0700

    Adding parameters to the script

--- tools/sanity_summary_tool.py
+++ tools/sanity_summary_tool.py
@@ -5,6 +5,7 @@ import paramiko
 import os
 import subprocess
 import urllib 
+from optparse import OptionParser
 
 def get_summary_():
 	sample_dict = {0: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_12_20_29_57/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': 'CentOS Linux release 7.1.1503 Core 3.0.2.0-26~kilo'}, 1: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_12_20_53_54/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': 'CentOS Linux release 7.1.1503 Core 3.0.2.0-26~juno'}, 2: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_12_22_17_02/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~juno'}, 3: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_12_22_17_10/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~juno'}, 4: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_12_22_18_06/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~juno'}, 5: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_00_14_37/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~juno'}, 6: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_00_15_00/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~juno'}, 7: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_00_15_15/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~liberty'}, 8: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_00_15_45/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~kilo'}, 9: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_00_52_44/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~kilo'}, 10: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_06_00_22/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~juno'}, 11: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_16_15_10/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~kilo'}, 12: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_13_16_50_21/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~kilo'}, 13: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_14_07_49_37/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~kilo'}, 14: {'report': 'http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_18_14_57_49/junit-noframes.html', 'build': '3.0.2.0-26', 'distro': '"Ubuntu 14.04.2 LTS" 3.0.2.0-26~kilo'}}
@@ -157,64 +158,119 @@ def print_summary():
 	rep_path = str(ini_dict["report"]).replace('http://10.204.216.50/', 'http://mayamruga.englab.juniper.net/')
 	print "Path to the html file (Paste this link on the browser): %s\n" % rep_path
 
-def get_all_ini_files():
+def get_exact_path(branch='', build=''):
+	branch= branch
+	build = build
+	host = '10.204.216.50'
+	username = 'bhushana'
+	password = 'bhu@123'
+	ini_list = []
+	err_list = []
+	path = "/var/www/html/sanity/daily/"
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(host, username = username, password = password)
+	stdin, stdout, stderr = client.exec_command('ls %s | grep %s' %(path, branch))
+	a = stdout.readlines()
+	for i in a:
+		b = i.replace('\n', '')
+		ini_list.append(b)
+	#err_list = stderr.readlines()
+	client.close()
+	if (len(ini_list) == 1):
+		path = path + ini_list[0] + '/'
+		#print path
+	else:
+		return None
+	ini_list = []
+	err_list = []
+	client = paramiko.SSHClient()
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	client.connect(host, username = username, password = password)
+	stdin, stdout, stderr = client.exec_command('ls %s | grep %s' %(path, build))
+	a = stdout.readlines()
+	for i in a:
+		b = i.replace('\n', '')
+		ini_list.append(b)
+	#err_list = stderr.readlines()
+	client.close()
+	if (len(ini_list) == 1):
+		path = path + ini_list[0]
+		#print path
+		return path
+
+
+def get_all_ini_files(branch='', build=''):
 	host = '10.204.216.50'
 	username = 'bhushana'
 	password = 'bhu@123'
 	ini_list = []
 	final_ini_list  = []
 	error_list = []
-	client = paramiko.SSHClient()
-	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-	client.connect(host, username = username, password = password)
-	stdin, stdout, stderr = client.exec_command('ls /var/www/html/Docs/sanity/daily/R3.0/3.0.2.0-26 | grep .ini')
-	ini_list =stdout.readlines()
-	error_list = stderr.readlines()
-	client.close()
-	for i in ini_list:
-		a = str(i).replace('\n', '')
-		final_ini_list.append(a)
-	#for i in final_ini_list:
-	#	print i
-	if (len(error_list)==0):
-		return final_ini_list
+	path = get_exact_path(branch=branch, build=build)
+	#print path
+	if path == None:
+		print "Branch name or Build Number is wrong, please check ..."
 	else:
-		return error_list 
+		client = paramiko.SSHClient()
+		client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		client.connect(host, username = username, password = password)
+		stdin, stdout, stderr = client.exec_command('ls %s | grep .ini' %path)
+		ini_list =stdout.readlines()
+		error_list = stderr.readlines()
+		client.close()
+		for i in ini_list:
+			a = str(i).replace('\n', '')
+			final_ini_list.append(a)
+		#for i in final_ini_list:
+		#	print i
+		if (len(error_list)==0):
+			return final_ini_list
+		else:
+			return error_list 
 
-def get_detailed_data_from_ini_files():
+
+def get_detailed_data_from_ini_files(branch='', build=''):
 	host = '10.204.216.50'
 	username = 'bhushana'
 	password = 'bhu@123'
 	info_dict = {}
-	path = '/var/www/html/Docs/sanity/daily/R3.0/3.0.2.0-26'
-	ini_list = get_all_ini_files()
-	if len(ini_list) == 1:
-		print "There is an Error Fetching all the ini files from the given path: \n %s " % ini_list[0]
+	path = str(get_exact_path(branch=branch, build=build))
+	#print path
+	if path == None:
+		print "The Branch name or build number is wrong please check"
 	else:
-		num = 0 
-		print "Downloading all the report.ini files in the given path and getting the reiquired data from them \n"
-		for i in ini_list:
-			new_path = path + '/'+ i
-			#print "Downloading the following file: ",new_path
-			os.system('sshpass -p "bhu@123" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no bhushana@10.204.216.50:%s .' %new_path)
-			Dict_1 = get_data_from_ini_file(i)
-			info_dict[num] = Dict_1
-			os.system('rm %s' %i)
-			#print "Done %d " %num
-			num += 1
-	#print info_dict
-	print "\n"
-	print "Adding the summary data to the already collected data from the ini files"
-	print "______________________________________________________________________________________"
-	print "RESULTS :-"
-	print "______________________________________________________________________________________"
-	for i in info_dict:
-		test_num_dict = get_summary_from_html_file(info_dict[i]["report"])
-		info_dict[i]["testcase_results"] = test_num_dict
-	return info_dict
 
-def print_report_summary():
-	info_dict = get_detailed_data_from_ini_files()
+		#path = '/var/www/html/Docs/sanity/daily/R3.0/3.0.2.0-26'
+		ini_list = get_all_ini_files(branch=branch, build= build)
+		#print ini_list
+		if len(ini_list) == 1:
+			print "There is an Error Fetching all the ini files from the given path: \n %s " % ini_list[0]
+		else:
+			num = 0 
+			print "Downloading all the report.ini files in the given path and getting the reiquired data from them \n"
+			for i in ini_list:
+				new_path = path + '/'+ i
+				#print "Downloading the following file: ",new_path
+				os.system('sshpass -p "bhu@123" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no bhushana@10.204.216.50:%s .' %new_path)
+				Dict_1 = get_data_from_ini_file(i)
+				info_dict[num] = Dict_1
+				os.system('rm %s' %i)
+				#print "Done %d " %num
+				num += 1
+		#print info_dict
+		print "\n"
+		print "Adding the summary data to the already collected data from the ini files"
+		print "______________________________________________________________________________________"
+		print "RESULTS :-"
+		print "______________________________________________________________________________________"
+		for i in info_dict:
+			test_num_dict = get_summary_from_html_file(info_dict[i]["report"])
+			info_dict[i]["testcase_results"] = test_num_dict
+		return info_dict
+
+def print_report_summary(branch='', build=''):
+	info_dict = get_detailed_data_from_ini_files(branch=branch, build=build)
 	for i in info_dict:
 		print "Distro : %s" % info_dict[i]["distro"]
 		print "Build : %s" % info_dict[i]["build"]
@@ -222,48 +278,6 @@ def print_report_summary():
 		topo_string = str(info_dict[i]["topology"])
 		replaced_topo_string = topo_string.replace('<br/>', '')
 		print "Topology : %s" % replaced_topo_string
-		"""
-		a_temp = replaced_topo_string.split(':')
-		#print a_temp
-		temp_list = ['Config Nodes', 'Control Nodes', 'Compute Nodes', 'Openstack Node', 'WebUI Node', 'Analytics Nodes', 'Physical Devices']
-		b_temp = []
-		for del_string in temp_list:
-			for rep_string in a_temp:
-				if del_string in rep_string:
-					a = rep_string.replace(del_string, '')
-					b_temp.append(a)
-		#print b_temp
-		c_temp = []
-		for j in b_temp:
-			a = j.replace(' ', '')
-			c_temp.append(a)
-		#print c_temp
-		d_temp = []
-		for j in c_temp:
-			a = j.replace("[u'", '')
-			d_temp.append(a)
-		#print d_temp
-		e_temp = []
-		for j in d_temp:
-			a = j.replace("'", '')
-			e_temp.append(a)
-		#print e_temp
-		f_temp = []
-		for j in e_temp:
-			a = j.replace("]", '')
-			f_temp.append(a)
-		del f_temp[0]
-		#print f_temp
-		first_element = f_temp[0]
-		itr = 0
-		for j in f_temp:
-			if j != first_element:
-				itr = 1
-		if itr == 0:
-			print "Topology Summary: SINGLE NODE CLUSTER"
-		else:
-			print "Topology Summary: MULTI-NODE CLUSTER"
-		"""
 		print "Topology Summaty: %s" %info_dict[i]['topo_summary']
 		test_result_dict_lables = info_dict[i]["testcase_results"]["labels"]
 		test_result_dict_values = info_dict[i]["testcase_results"]["values"]
@@ -278,8 +292,8 @@ def print_report_summary():
 		print "______________________________________________________________________________________"
 
 
-def get_html_file():
-	info_dict = get_detailed_data_from_ini_files()
+def get_html_file(dest_file='', branch='', build=''):
+	info_dict = get_detailed_data_from_ini_files(branch=branch, build=build)
 	html_string = '''
 	<!DOCTYPE html>
 	<html>
@@ -310,6 +324,7 @@ def get_html_file():
 			<th> DISTRO </th>
 			<th> BUILD </th>
 			<th> REPORT LINK </th>
+			<th> TOPOLOGY </th>
 			<th> TEST SUMMARY </th>
 		</tr>\n
 	'''
@@ -319,7 +334,8 @@ def get_html_file():
 			<th> %s </th>
 			<th> %s </th>
 			<th> <a href="%s"> click here </th>
-		''' % (info_dict[i]["distro"], info_dict[i]["build"], info_dict[i]["report"])
+			<th> %s </th>
+		''' % (info_dict[i]["distro"], info_dict[i]["build"], info_dict[i]["report"], info_dict[i]['topo_summary'])
 		summ = ''
 		test_result_dict_lables = info_dict[i]["testcase_results"]["labels"]
 		test_result_dict_values = info_dict[i]["testcase_results"]["values"]
@@ -337,15 +353,42 @@ def get_html_file():
 	</body>
 	</html>
 	"""
-	print html_string
+	#print html_string
+	if dest_file == '':
+		f.open('result.html', 'w')
+		f.write(html_string)
+		f.close()
+	else:
+		f = open(dest_file, 'w')
+		f.write(html_string)
+		f.close()
+
+def main():
+	parser = OptionParser()
+	parser.add_option('-d','--dest_file_name', help="Give a name to the destination html file", type="string", dest='dest_file_name')
+	parser.add_option('--branch', help="Branch Name", type="string", dest="branch")
+	parser.add_option('--build', help="Build Number", type="string", dest="build")
+	parser.add_option('-o', '--output_format', help="Print output / get html file (print/html)", type="string", dest="out_format")
+	(opts, args) = parser.parse_args()
+	print opts
+	if opts.out_format == "print":
+		print_report_summary(branch=opts.branch, build=opts.build)
+	elif opts.out_format == "html":
+		get_html_file(dest_file=opts.dest_file_name, branch=opts.branch, build=opts.build)
+	else:
+		print " Wrong Option Selected. Please select 'print/html'"
+
+	#get_html_file(dest_file=opts.dest_file_name)
 
 
+
+main()
+#get_exact_path()
 #-o UserKnownHostsFile=/dev/null 
-
 #a = get_summary_from_html_file('http://10.204.216.50/Docs/logs/3.0.2.0-26_2016_04_12_20_29_57/junit-noframes.html')
 #print a
 #a = get_detailed_data_from_ini_files()
-print_report_summary()
+#print_report_summary()
 #get_html_file()
 #a=get_all_ini_files()
 #print a
