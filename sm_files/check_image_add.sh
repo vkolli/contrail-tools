a9281a4550d516a2a8284f638d6da16fc9bfa9d0
VIVEK GARG <vivekgarg@juniper.net>
2017-04-29 12:57:55 -0700

    Change add contrail package timeout

--- sm_files/check_image_add.sh
+++ sm_files/check_image_add.sh
@@ -4,13 +4,13 @@ set -e
 image_name=$1
 count=0
 while [ $(server-manager display image --image_id $image_name --json | grep -c  id) -ne 1 ]; do
-    if [ "$count"  -ne 30 ]; then
+    if [ "$count"  -ne 120 ]; then
         echo "Image is not added yet, lets wait for some more time"
         sleep 30
         count=$((count+1))
         echo "true" > /tmp/image_add
     else
-        echo "waited for 900 seconds, but imgae add didn't go through, exiting"
+        echo "waited for 60 minutes, but imgae add didn't go through, exiting"
         echo "false" > /tmp/image_add
         #break
         exit 1
