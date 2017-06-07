f83571e0f10fa32d99ea0860a5725c102d8558f6
Sudheendra Rao <sudheendra@nodem4.englab.juniper.net>
2017-06-07 16:59:33 +0530

    changes required for 16.04

--- testbeds/testbed_i21_i26.py
+++ testbeds/testbed_i21_i26.py
@@ -16,13 +16,11 @@ host_build = 'stack@10.204.216.49'
 
 env.roledefs = {
     'all': [host1, host2, host3,host4,host5, host6],
-    'cfgm': [host1],
-    'webui': [host1],
-    'openstack': [host2],
-    'control': [host1, host3],
-    'collector': [host1],
-    'database': [host1],
-    'compute': [host4, host5, host6],
+    'contrail-controller': [host1, host2, host3],
+    'openstack': [host4],
+    'contrail-analytics': [host1, host2, host3],
+    'contrail-analyticsdb': [host1, host2, host3],
+    'contrail-compute': [host5,host6],
     'build': [host_build]
 }
 
@@ -71,27 +69,6 @@ env.ostypes = {
 }
 
 env.openstack_admin_password = 'c0ntrail123'
-env.keystone = {
-    'keystone_ip'   : '10.204.217.134',
-    'auth_protocol' : 'http',                  #Default is http
-    'auth_port'     : '35357',                 #Default is 35357
-    'admin_token' : 'caa9e2054d04497d87e2d6fb0b1edeea',
-    'admin_user'    : 'admin',                 #Default is admin
-    'admin_password': 'c0ntrail123',           #Default is contrail123
-    'nova_password'   : 'contrail123',           #Default is the password set in admin_password
-    'neutron_password': 'contrail123',           #Default is the password set in admin_password
-    'service_tenant': 'service',               #Default is service
-    'admin_tenant'  : 'admin',                 #Default is admin
-    'region_name'   : 'RegionOne',             #Default is RegionOne
-    'insecure'      : 'True',                  #Default = False
-    'manage_neutron'  : 'no',                #Default = 'yes',Does configure neutron user/role in keystone required.
-}
-
-env.openstack = {
-    'service_token' : 'caa9e2054d04497d87e2d6fb0b1edeea',
-    'amqp_host' : '10.204.217.134',
-}
-
 
 env.cluster_id='i21_i26_cluster'
 minimum_diskGB=32
@@ -105,6 +82,5 @@ env.encap_priority =  "'VXLAN','MPLSoUDP','MPLSoGRE'"
 
 enable_ceilometer = True
 ceilometer_polling_interval = 60
-env.log_scenario='Multi-Node Sanity'
-#env.log_scenario='Multi-Interface Sanity[mgmt, ctrl=data]'
+env.log_scenario='SMLite Container Multi-Node ContrailHA Sanity'
 env.enable_lbaas = True
