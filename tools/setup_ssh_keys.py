62b6b606925b313623e997d4edda9bc0511a0a30
Vedu Joshi <vjoshi@juniper.net>
2017-06-16 00:13:56 +0530

    Fix bug in setup_ssh_keys.py

--- tools/setup_ssh_keys.py
+++ tools/setup_ssh_keys.py
@@ -1,4 +1,4 @@
-#-----------------
+#----------------- 
 # From the testbed file, enable automatic ssh login
 # usage: python setup_ssh_keys.py <path of testbed.py>
 #----------------
@@ -11,7 +11,6 @@ testbed_path = sys.argv[1]
 testbed = imp.load_source('testbed', testbed_path)
 
 env.disable_known_hosts = True
-
 # Read pub file content
 pub_file = os.path.expanduser('~/.ssh/id_rsa.pub')
 if not os.path.isfile(pub_file):
@@ -21,18 +20,20 @@ id_rsa_pub = open('%s' %(pub_file), 'r').read().strip()
 
 for host_string in testbed.env.roledefs['all']:
     (u, host) = host_string.split('@')
-    with settings(host_string=host_string):
-        run('ls')
-        sudo('mkdir -p .ssh ; chmod 700 .ssh')
+    with settings(host_string=host_string, password=env.passwords[host_string]):
+        run('mkdir -p .ssh ; chmod 700 .ssh')
 
 		# Ensure that pub key is present
         auth_file = '~/.ssh/authorized_keys'
-        sudo('grep -q -F "%s" %s || echo "%s" >> %s' %(id_rsa_pub, auth_file,
+        run('grep -q -F "%s" %s || echo "%s" >> %s' %(id_rsa_pub, auth_file,
             id_rsa_pub, auth_file))
 
 		# Check and update known_hosts
         keyscan = local('ssh-keyscan -H %s' %(host), capture=True)
-        known_hosts = open(os.path.expanduser('~/.ssh/known_hosts'), 'r').read()
+
+        known_hosts = ''
+        if os.path.isfile('~/.ssh/known_hosts'):
+            known_hosts = open(os.path.expanduser('~/.ssh/known_hosts'), 'r').read()
         if not keyscan in known_hosts:
             print 'Adding host %s in known_hosts' %(host)
             local('echo "%s" >> ~/.ssh/known_hosts' %(keyscan))
