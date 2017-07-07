0c3140783367135fd3a200eb2f8cbe0535d0160a
Sudheendra Rao <sudheendra@nodem4.englab.juniper.net>
2017-07-07 12:12:50 +0530

    converting the testbed to container based format

--- testbeds/testbed_k12_webui.py
+++ testbeds/testbed_k12_webui.py
@@ -16,13 +16,11 @@ public_vn_subnet = "10.84.41.0/24"
 host_build = 'stack@10.204.216.49'
 env.roledefs = {
     'all': [host1],
-    'cfgm': [host1],
+    'contrail-controller': [host1],
     'openstack':[host1],
-    'collector': [host1],
-    'webui': [host1],
-    'control': [host1],
-    'compute': [host1],
-    'database': [host1],
+    'contrail-analytics': [host1],
+    'contrail-analyticsdb': [host1],
+    'contrail-compute': [host1],
     'build': [host_build],
 }
 env.hostnames = {
@@ -35,6 +33,11 @@ env.ostypes = {
 #Openstack admin password
 env.openstack_admin_password = 'contrail123'
 
+env.kernel_upgrade=False
+env.openstack = {
+    'manage_amqp': "true"
+}
+
 env.passwords = {
     host1: 'c0ntrail123',
     host_build: 'stack@123',
@@ -44,5 +47,3 @@ env.test_repo_dir='/home/stack/webui_ubuntu_single_node/icehouse/contrail-test'
 env.mail_from='pavanap@juniper.net'
 env.mail_to='dl-contrail-sw@juniper.net'
 env.log_scenario ='Single Node Webui Sanity'
-
-
