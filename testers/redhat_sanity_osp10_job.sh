5a75d8b2b1e0253e78dfa065519b9560bfbda6fd
Shaju V K <shajuvk@juniper.net>
2017-07-18 12:19:43 -0700

    TEST_CONTAINER_IMAGE_DIR changed for redhat osp10

--- testers/redhat_sanity_osp10_job.sh
+++ testers/redhat_sanity_osp10_job.sh
@@ -56,7 +56,7 @@ run_build_fab "osp10_sanity" || debug_and_die "Failed during osp10 provisioning"
 exec_cmds -s ${TASK_RUNNER_HOST_STRING} -p ${TASK_RUNNER_HOST_PASSWORD} -c "sshpass -p $API_SERVER_HOST_PASSWORD scp $PKG_FILE_DIR/artifacts/contrail-install-packages*.tgz  ${UNDERCLOUD_HOST_STRING}:/var/www/html/contrail/" || die "Failed to copy contrail-install-packages  tgz to $UNDERCLOUD_NODEHOME:"
 
 #run_build_fab "osp10_instack_and_templates"
-exec_cmds -s ${TASK_RUNNER_HOST_STRING} -p ${TASK_RUNNER_HOST_PASSWORD} -c "sshpass -p $API_SERVER_HOST_PASSWORD scp $PKG_FILE_DIR/artifacts/contrail-install-packages*.tgz  ${UNDERCLOUD_HOST_STRING}:/var/www/html/contrail/" || die "Failed to copy contrail-install-packages  tgz to $CONTRAIL_REPO_UNDERCLOUD:"
+exec_cmds -s ${TASK_RUNNER_HOST_STRING} -p ${TASK_RUNNER_HOST_PASSWORD} -c "sshpass -p $API_SERVER_HOST_PASSWORD scp $PKG_FILE_DIR/artifacts/contrail-thirdparty-packages*.tgz  ${UNDERCLOUD_HOST_STRING}:/var/www/html/contrail/" || die "Failed to copy contrail-install-packages  tgz to $CONTRAIL_REPO_UNDERCLOUD:"
 
 run_build_fab "osp_deploy" || debug_and_die "osp deployment task failed"
 
