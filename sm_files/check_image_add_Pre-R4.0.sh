33a36764551617c6017a5441441c4bc5321d2f4b
Jeba Paulaiyan <jebap@juniper.net>
2017-08-18 12:33:23 -0700

    Adding check_image_add_Pre-R4.0.sh

---
+++ sm_files/check_image_add_Pre-R4.0.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+set -e
+image_name=$1
+count=0
+while [ $(server-manager show image --image_id $image_name | grep -c  id) -ne 1 ]; do
+    if [ "$count"  -ne 120 ]; then
+        echo "Image is not added yet, lets wait for some more time"
+        sleep 30
+        count=$((count+1))
+        echo "true" > /tmp/image_add
+    else
+        echo "waited for 60 minutes, but imgae add didn't go through, exiting"
+        echo "false" > /tmp/image_add
+        #break
+        exit 1
+    fi
+done
+echo "true" > /tmp/image_add
+sleep 60
