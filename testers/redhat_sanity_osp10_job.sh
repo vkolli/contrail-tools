1b7c22ad207c92f977bde38d6ab6be08d17f51ed
Shaju V K <shajuvk@juniper.net>
2017-09-26 00:21:29 -0700

    added copying of test repo

--- testers/redhat_sanity_osp10_job.sh
+++ testers/redhat_sanity_osp10_job.sh
@@ -60,6 +60,7 @@ run_build_fab "osp10_sanity" || debug_and_die "Failed during osp10 provisioning"
 
 exec_cmds -s ${TASK_RUNNER_HOST_STRING} -p ${TASK_RUNNER_HOST_PASSWORD} -c "sshpass -p $API_SERVER_HOST_PASSWORD scp $PKG_FILE_DIR/artifacts/contrail-install-packages*.tgz  ${UNDERCLOUD_HOST_STRING}:/var/www/html/contrail/" || die "Failed to copy contrail-install-packages  tgz to $UNDERCLOUD_NODEHOME:"
 
+sshpass -p 'c0ntrail123' scp -r ${SSHOPT} $PKG_FILE_DIR/artifacts_extra/contrail-test-*.tgz ${TEST_VM_HOST_STRING}:${TEST_HOST_HOME}
 #run_build_fab "osp10_instack_and_templates"
 # ***Enbale copying of docker image once we support contrainer in redhat****
 #exec_cmds -s ${TASK_RUNNER_HOST_STRING} -p ${TASK_RUNNER_HOST_PASSWORD} -c "sshpass -p ${API_SERVER_HOST_PASSWORD} scp $CONTRAIL_NETWORKING_DOCKER_IMG_DIR/contrail-docker-images_*.tgz  ${UNDERCLOUD_HOST_STRING}:/var/www/html/contrail/" || die "Failed to copy contrail-install-packages  tgz to $CONTRAIL_REPO_UNDERCLOUD:"
