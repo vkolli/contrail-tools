1bab548bf87881d930978d876309029e0cb76a22
Pulkit Tandon <pulkitt@juniper.net>
2017-12-06 11:51:52 +0530

    Added external access IPs in testbed file - K8s

--- testbeds/testbed_kube_pulkit_setup_ha_cntrl_data.py
+++ testbeds/testbed_kube_pulkit_setup_ha_cntrl_data.py
@@ -12,10 +12,10 @@ host5 = 'root@10.204.217.100'
 #External routers if any
 #for eg. 
 #ext_routers = [('mx1', '10.204.216.253')]
-#ext_routers = [('hooper', '192.168.1.11')]
-router_asn = 64510
-#public_vn_rtgt = 10003
-#public_vn_subnet = "10.204.219.56/29"
+ext_routers = [('hooper', '77.77.1.100')]
+router_asn = 64512
+public_vn_rtgt = 12005
+public_vn_subnet = "10.204.219.136/29"
 
 
 #Host from which the fab commands are triggered to install and provision
