3f3e7cc7bd2672907d24bd5cad978e6ea89bb951
usha <musharani@juniper.net>
2017-12-28 16:10:14 +0530

    Adding nodec41 and nodec59 testbed files

---
+++ testbeds/testbed_nodec41.py
@@ -0,0 +1,52 @@
+from fabric.api import env
+
+os_username = 'admin'
+os_password = 'contrail123'
+os_tenant_name = 'demo'
+#multi_tenancy = True
+webui = True
+horizon = False
+ui_config = 'contrail'
+ui_browser = 'firefox'
+host1='root@10.204.217.81'
+ext_routers = []
+router_asn = 64512
+public_vn_rtgt = 10000
+public_vn_subnet = "10.84.41.0/24"
+host_build = 'stack@10.204.216.49'
+env.roledefs = {
+    'all': [host1],
+    'contrail-controller': [host1],
+    'openstack':[host1],
+    'contrail-analytics': [host1],
+    'contrail-analyticsdb': [host1],
+    'contrail-compute': [host1],
+    'build': [host_build],
+}
+env.hostnames = {
+    'all': ['nodec41']
+}
+env.ostypes = {
+     host1: 'ubuntu'
+}
+
+#Openstack admin password
+env.openstack_admin_password = 'contrail123'
+
+env.kernel_upgrade=False
+env.openstack = {
+    'manage_amqp': "true"
+}
+
+env.keystone = {
+    'admin_password': 'contrail123'
+}
+
+env.passwords = {
+    host1: 'c0ntrail123',
+    host_build: 'stack@123',
+}
+minimum_diskGB=32
+env.mail_from='musharani@juniper.net'
+env.mail_to='musharani@juniper.net'
+env.log_scenario ='Single Node Webui Sanity'
