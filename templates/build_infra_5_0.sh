376fe73423d3cdbaf35d2bd63acb316dc508a8de
pryadav <pryadav@juniper.net>
2018-03-23 14:16:29 -0700

    added check for OS type

--- templates/build_infra_5_0.sh
+++ templates/build_infra_5_0.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 source /etc/contrail/openstackrc
 echo $1 >> /root/$1/info.txt
 a="$(openstack project create $1 -f json > /root/$1/uuid.json)"
@@ -134,9 +135,29 @@ then
 	else
 	    echo "The confing node ip where the contrail-deployments repo is going to cloned is: "$config_node_ip
 	    sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /root/$1/ansible root@$config_node_ip:/root/ansible
-	    sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'yum install -y git ansible epel-release vim'
-	    sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /root/$1/all.yml root@$config_node_ip:/root/ansible/inventory/group_vars/
-	    sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'cd /root/ansible/ ; ansible-playbook -i inventory/ playbooks/all.yml'
+        OS_INFO=`sshpass -p 'c0ntrail123' ssh -o 'StrictHostKeyChecking no' -o 'UserKnownHostsFile /dev/null' root@$config_node_ip 'cat /etc/*elease | grep "PRETTY_NAME"'`
+        OS=unknown
+        if [[ $OS_TYPE = *"CentOS"* ]]; then
+        OS="centos"
+        elif [[ $OS_TYPE = *"Ubuntu"* ]]; then
+        OS="ubuntu"
+        fi
+        echo $OS
+        if $OS == "centos"; then
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'yum install -y git ansible epel-release vim'
+	        sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /root/$1/all.yml root@$config_node_ip:/root/ansible/inventory/group_vars/
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'cd /root/ansible/ ; ansible-playbook -i inventory/ playbooks/all.yml'
+	    elif $OS=="ubuntu"
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'apt update'
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'apt upgrade'
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'apt install software-properties-common'
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'apt-add-repository ppa:ansible/ansible'
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'apt install sshpass ansible'
+
+	        sshpass -p c0ntrail123 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null /root/$1/all.yml root@$config_node_ip:/root/ansible/inventory/group_vars/
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'cd /root/ansible/ ; ansible-playbook -i inventory/ playbooks/install.yml'
+	        sshpass -p c0ntrail123 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$config_node_ip 'cd /root/ansible/ ; ansible-playbook -i inventory/ playbooks/all.yml
+	    fi
 	fi
 else
         echo "Network Stack Creation failed. So creation of the SERVER STACK is TERMINATED !!!!"
