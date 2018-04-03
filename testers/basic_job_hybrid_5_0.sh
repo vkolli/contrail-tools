b00c9ac90914b6e78d9396a479a76e33984eed8f
salikapati <salikapati@juniper.net>
2018-04-02 17:27:03 -0700

    changes to support dpdk 5.0 jenkins job

---
+++ testers/basic_job_hybrid_5_0.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TOOLS_WS=${TOOLS_WS:-$(pwd)}
+source $TOOLS_WS/testers/utils
+source $TOOLS_WS/testers/utils_virtual
+testbeds=(${AVAILABLE_TESTBEDS//,/ })
+# AVAILABLE_TESTBEDS is a comma separated list of testbed filenames or paths
+get_testbed
+launch_testbed_hybrid_5_0
+unlock_testbed $TBFILE_NAME
